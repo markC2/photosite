b84972f5fb1d7da69695a88ddfbf68556438fffa
careym04 <mark.carey@bbc.co.uk>
2024-03-23 13:09:18 +0000

    fixing styling

--- photosite_project/photos/urls.py
+++ photosite_project/photos/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from photos import views
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 app_name = 'photos'
 
 urlpatterns = [
     path('', views.index, name='index')
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
